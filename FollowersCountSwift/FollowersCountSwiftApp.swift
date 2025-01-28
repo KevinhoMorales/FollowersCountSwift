d7dc08247d7f8fba8352847d35eee26997b036f1
Kevin Morales <kfmorales94@gmail.com>
2025-01-27 21:51:34 -0500

    Project Init

---
+++ FollowersCountSwift/FollowersCountSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  FollowersCountSwiftApp.swift
+//  FollowersCountSwift
+//
+//  Created by Kevinho Morales on 27/1/25.
+//
+
+import SwiftUI
+
+@main
+struct FollowersCountSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
